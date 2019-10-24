a00d7a2cbd16d6e1a98b6dedcae37a3282e1be82
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2019-10-25 00:40:48 +0300

    #lesson5

--- src/main/java/com/javaguru/todolist/service/validation/TaskValidationService.java
+++ src/main/java/com/javaguru/todolist/service/validation/TaskValidationService.java
@@ -1,13 +1,19 @@
 package com.javaguru.todolist.service.validation;
 
 import com.javaguru.todolist.domain.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Set;
 
+@Component
 public class TaskValidationService {
 
     private final Set<TaskValidationRule> validationRules;
 
+    @Autowired
     public TaskValidationService(Set<TaskValidationRule> validationRules) {
         this.validationRules = validationRules;
     }
